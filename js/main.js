a99ed0643ec0af8dc8883c7b2470b3cd34358360
Aaron Wagner <wagner@e-spirit.com>
2021-05-19 18:27:41 +0200

    you can now add elements

--- js/main.js
+++ js/main.js
@@ -3,43 +3,67 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     let inputText = document.getElementById("inputText")
-    let list = document.getElementById("list")
+    const all_todos = []
 
     inputText.addEventListener("keypress", (event) => {
         if (event.keyCode === 13) {
             event.preventDefault()
 
-            // create List-Element
-            const div = document.createElement("div")
-            const checkbox = document.createElement("input")
-            checkbox.type = "checkbox"
-            checkbox.classList.add("list-box")
-
-            const text = document.createTextNode(inputText.value)
-
-            checkbox.addEventListener("click", () => {
-                text.classList.add("strike")
-
-                // calculate open todos
-                const count = document.getElementById("count")
-
-                for (let liElement of list.children) {
-                    alert("liElement")
+            all_todos.push(
+                {
+                    text: inputText.value,
+                    checked: false
                 }
-
-                count.innerText = "There are " + "0" + " things to do"
-
-            })
-
-            div.appendChild(checkbox)
-
-            div.appendChild(text)
-            const li = document.createElement("li")
-            li.appendChild(div)
-
-            // insert to list
-            list.appendChild(li)
+            )
+            updateUI()
+            inputText.value = ""
+            console.log(all_todos)
         }
     })
+
+    function updateUI() {
+        // clear all li-elements
+        let list = document.getElementById("list")
+
+        while(list.firstChild) list.removeChild(list.firstChild);
+
+        let openCount = 0
+
+        // todos-liste durchgehen und neue lis erzeugen
+        for (let i = 0; i < all_todos.length; i++) {
+            const liElement = createLiElement(all_todos[i])
+            // insert to list
+            list.appendChild(liElement)
+
+            if (!all_todos[i].checked) {
+                openCount++
+            }
+        }
+
+        // update open todos
+        const count = document.getElementById("count")
+        count.innerText = "There are " + openCount + " things to do"
+    }
+
+    function createLiElement(todo) {
+        const div = document.createElement("div")
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.classList.add("list-box")
+        checkbox.checked = todo.checked
+
+        const text = document.createTextNode(todo.text)
+
+ /*       checkbox.addEventListener("click", () => {
+            text.classList.add("strike")
+        })*/
+
+        div.appendChild(checkbox)
+
+        div.appendChild(text)
+        const li = document.createElement("li")
+        li.appendChild(div)
+        return li
+    }
 
 })
